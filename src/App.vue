c905c8a084b5300a15ed957fb690914896eade3d
gtamani <72049315+gtamani@users.noreply.github.com>
2021-07-18 09:51:40 -0300

    added-opponent-cursor
    .

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,125 @@
 <template>
-  <div id="app" v-on:keydown="keyPressed">
-    <div id="clock" v-if="show">
-        {{minutes}} : {{seconds}}      - {{speed}}
+<div id="app" class="d-flex flex-column justify-content-center align-items-center" v-on:keydown="keyPressed">
+    <div id='container' class="p-2">
+        <div id="clock" v-if="show">
+            {{minutes}} : {{seconds}}      - {{speed}}
+        </div>
+        <button @click="changeVisibility">Show/Don't show next letter</button>
+        <div>
+        <prompter
+        :text='text'
+        :opponent='opponentPosition'
+        :well='position'
+        :wrong='lettersToPosition'/>
+        </div>
+        <div v-if="show">
+            {{ nextLetter }} - {{position}} - {{lettersToPosition}} - {{words}} - {{opponentPosition}}
+        </div>
+        <input type="text">
     </div>
-    <button @click="changeVisibility">Show/Don't show next letter</button>
-    <div>
-      <prompter 
-      :text = "text"
-      :well = "position"
-      :wrong = "lettersToPosition"/>
-    </div>
-    <div v-if="show">
-        {{ nextLetter }} - {{position}} - {{lettersToPosition}} - {{words}}
-    </div>
-    <input type="text">
-  </div>
+</div>
 </template>
 
 <script>
 import Prompter from './components/Prompter'
 export default {
-  name: 'App',
-  components: {
-    Prompter
-  },
-  data () {
-    return {
-      // Text data
-      show: true,
-      text: 'Sin embargo, la posibilidad de utilizar publicidad en Wikipedia provocó una reacción de parte de los contribuyentes de la Wikipedia en español, motivo que llevó a la creación de la Enciclopedia Libre Universal en Español en febrero de 2002.',
-      position: 0,
-      lettersToPosition: 0,
-      words: 0,
-      started: new Date(Date.now()),
-      unvalidKeyCodes: [17,16,20,9,18,27,222,13],
-      sameLetter: false,
-      // Clock data
-      minutes: 0,
-      seconds: 0,
-      miliseconds: 0,
-      difference: 0,
-      speed: 0
+    name: 'App',
+    components: {
+        Prompter
+    },
+    data () {
+        return {
+            // Text data
+            show: true,
+            text: 'Sin embargo, la posibilidad de utilizar publicidad en Wikipedia provocó una reacción de parte de los contribuyentes de la Wikipedia en español, motivo que llevó a la creación de la Enciclopedia Libre Universal en Español en febrero de 2002.',
+            position: 0,
+            lettersToPosition: 0,
+            words: 0,
+            started: new Date(Date.now()),
+            unvalidKeyCodes: [17, 16, 20, 9, 18, 27, 222, 13],
+            sameLetter: false,
+            // Opponent
+            opponentPosition: 0,
+            opponentSpeed:40,
+            // Clock data
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0,
+            difference: 0,
+            speed: 0
+        }
+    },
+    methods: {
+        changeVisibility: function () {
+            this.show = !this.show
+        },
+        keyPressed: function (e) {
+            console.log('token')
+            this.sameLetter = e.key === this.nextLetter;
+            if (this.unvalidKeyCodes.indexOf(e.keyCode) == -1) {
+                if (this.sameLetter && this.lettersToPosition == 0) {
+                    this.position += 1;
+                    const next = this.text[this.position];
+                    if (next == " ") {this.words += 1}
+                    this.nextLetter = next;
+                } else {
+                    if (e.key === "Backspace") {
+                        if (this.position >= 0) {
+                            if (this.lettersToPosition === 0) {
+                                this.position -= 1;
+                                const next = this.text[this.position];
+                                this.nextLetter = next;
+                            } else {
+                                this.lettersToPosition -= 1;
+                            }
+                        }
+                    } else {
+                        this.lettersToPosition += 1;
+                    }
+                }
+            }
+        },
+        update: function () {
+            let date = new Date(Date.now()) 
+            this.difference = date.getTime() - this.started.getTime()
+            this.minutes = parseInt(this.difference / 60000)
+            this.seconds = (this.difference - this.minutes*60000) / 1000
+            this.speed = 60000 / (this.difference / this.words)
+        },
+        loop: function () {
+            this.nextLetter = this.text[0]
+            setInterval(() => {
+                this.update()
+            }, 15)
+        },
+        opponentLoop: function () {
+            setInterval(() => {
+                this.opponentPosition ++;
+            }, 60000 / this.opponentSpeed)
+        }
+    },
+    created () {
+        this.loop();
+        this.opponentLoop();
     }
-  },
-  methods: {
-      changeVisibility: function() {
-          this.show = !this.show
-      },
-      keyPressed: function(e) {
-          console.log("token");
-          this.sameLetter = e.key === this.nextLetter ? true : false;
-          if (this.unvalidKeyCodes.indexOf(e.keyCode) == -1) {
-              if (this.sameLetter && this.lettersToPosition == 0) {
-                  this.position += 1;
-                  const next = this.text[this.position];
-                  if (next == " ") {this.words += 1}
-                  this.nextLetter = next;
-              } else {
-                  if (e.key == "Backspace") {
-                      if (this.position >= 0) {
-                          if (this.lettersToPosition == 0) {
-                              this.position -= 1;
-                              const next = this.text[this.position];
-                              this.nextLetter = next;
-                          } else {
-                              this.lettersToPosition -= 1;
-                          }
-                      }
-                  } else {
-                      this.lettersToPosition += 1;
-                  }
-              }
-          }
-      },
-      update: function() {
-          let date = new Date(Date.now()) 
-          this.difference = date.getTime() - this.started.getTime();
-          this.minutes = parseInt(this.difference / 60000);
-          this.seconds = (this.difference-this.minutes*60000) / 1000;
-          this.speed = 60000 / (this.difference/this.words)
-      },
-      loop: function() {
-          this.nextLetter = this.text[0]
-          setInterval(() => {
-              this.update()
-          }, 15);
-      }
-  },
-  created () {
-      this.loop();
-  }
 }
 </script>
 
 <style>
+*{
+    margin:0;
+    padding:0;
+}
+#app{
+    width:100vw;
+    height:100vh;
+}
+#container{
+    width:80%;
+    height: 40%;
+    border:1px solid black;
+    text-align: center;
+}
 #red{
     background-color: red;
 }
--- src/components/Prompter.vue
+++ src/components/Prompter.vue
@@ -1,25 +1,65 @@
 <template>
-
-
-<div v-html="renderized"></div>
-
+<div id="prompter" v-html="renderized"></div>
 </template>
 
 <script>
 export default {
     name: 'Prompter',
     props: {
-        text: String,
+        text: {
+            type: String,
+        },
+        opponent: Number,
         well: Number,
         wrong: Number,
     },
-    template: "<div v-html = renderized></div>",
+    data () {
+        return {
+            bar: "<span id='bar'></span>",
+            green:"",
+            red:"",
+            yellow:"",
+            result:""
+        }
+    },
+    methods: {
+        add_cursor: function () {
+            let point1 = this.well;
+            let point2 = this.well+this.wrong;
+            let point3 = this.text.length+1;
+            //
+            this.green = this.text.slice(0,point1);
+            this.red = this.text.slice(point1,point2);
+            this.left = this.text.slice(point2,point3);
+            //
+            if (this.opponent > point2){
+                let opponent = this.opponent - point2
+                this.left = this.left.slice(0,opponent) + this.bar + this.left.slice(opponent,this.left.length)
+            } else if (this.opponent > point1) {
+                let opponent = this.opponent - point1
+                this.red = this.red.slice(0,opponent)+this.bar+this.red.slice(opponent,this.red.length)
+            } else {
+                this.green = this.green.slice(0,this.opponent)+this.bar+this.green.slice(this.opponent,this.green.length)
+            }
+        }
+    },
     computed: {
         renderized(){
-            return "<span id='green'>"+this.text.slice(0,this.well)+"</span><span id='red'>"+this.text.slice(this.well,this.well+this.wrong)+"</span>"+this.text.slice(this.well+this.wrong,this.text.length+1);
+            this.add_cursor()
+            return "<span id='green'>"+this.green+"</span><span id='red'>"+this.red+"</span>"+this.left;
         }
     }
 }
 </script>
 
+<style>
+#prompter{
+    border:1px solid black;
+    padding:4rem;
+}
+#bar{
+    border-left: 1px solid black;
+    animation: spark 1s infinite;
+}
 
+</style>
